3427b8a7f30661266c7477d7a5a11c0b310a56fd
MD AEJAZ AHMED <mdaejazahmed6692@gmail.com>
2024-01-15 18:39:27 +0530

    weekly test project

---
+++ script.js
@@ -0,0 +1,96 @@
+
+        const products = [
+            { id: 1, name: 'product-1', price: 100, quantity: 0},
+            { id: 2, name: 'product-2', price: 200, quantity: 0},
+            { id: 3, name: 'product-3', price: 300, quantity: 0},
+        ];
+        
+        const productList = document.getElementById('product-list');
+        const cartList = document.getElementById('cart-list');
+        const toatlPriceElement = document.getElementById('total-price');
+
+     window.onload = (()=>{
+        renderProducts();
+        renderCart();
+     })
+
+        let cart = [];
+        function renderProducts(){
+            productList.innerHTML = '';
+            products.forEach((product) => {
+                const listItem = document.createElement('div');
+                listItem.innerHTML = `<p>${product.name}</p> <p>${product.price}</p>
+                    <div class="btn"><span onclick="decrementQuantity(${product.id})" class="decrement"> - </span><span>${product.quantity}</span><span onclick="incrementQuantity(${product.id})" class="decrement"> + </span></div>`;
+                    listItem.style.display = "flex";
+                    listItem.style.width = "40vw";
+                    listItem.style.margin = "8px";
+                    listItem.style.justifyContent = "space-around";
+                    listItem.style.alignItems = "center";
+                listItem.style.backgroundColor = "rgba(178, 178, 187, 0.752)";
+                productList.appendChild(listItem);
+            });
+        }
+
+        function renderCart(){
+            cartList.innerHTML = '';
+             
+            let totalPrice = 0;
+
+            if(cart.length === 0){
+                const noproductmessage = document.createElement('p');
+                noproductmessage.innerText = 'No Product added to the cart';
+                cartList.appendChild(noproductmessage);
+            }else{
+                cart.forEach((cartItem) => {
+                    const listItem = document.createElement('div');
+                    listItem.innerHTML = `<p>${cartItem.name}</p> <p>${cartItem.quantity} x ${cartItem.price}</p>`;
+                    listItem.style.margin = "8px";
+                    listItem.style.backgroundColor = "rgba(178, 178, 187, 0.752)";
+                    listItem.style.justifyContent = "space-around";
+                    listItem.style.alignItems = "center";
+                    listItem.style.display = "flex";
+                    listItem.style.width = "20vw";
+                    cartList.appendChild(listItem);
+                    totalPrice += cartItem.price * cartItem.quantity;
+                })
+            }
+            toatlPriceElement.innerText = `Total Price: ${totalPrice}`;
+        }
+
+        function addtocart(product){
+            const existingCartItem = cart.find((item) => item.id === product.id);
+            if(existingCartItem){
+                existingCartItem.quantity++;
+            }else{
+                cart.push({ ...product, quantity: 1 });
+            }
+            renderProducts();
+            renderCart();
+        }
+
+        function incrementQuantity(productId){
+            const Product = products.find((p) => p.id === productId);
+            if(Product){
+                Product.quantity++;
+                addtocart(Product);
+            }
+        }
+        function decrementQuantity(productId){
+            const Product = products.find((p) => p.id === productId);
+            if(Product && Product.quantity>0){
+                Product.quantity--;
+                const existingCartItem = cart.find((cartItems) => cartItems.id === productId);
+                if(existingCartItem){
+                    existingCartItem.quantity--;
+                    if(existingCartItem.quantity === 0){
+                        cart = cart.filter((item) => item.id !== productId);
+                        console.log(cart.length);
+                    }
+                }
+                renderProducts();
+                renderCart();
+            } 
+        }
+        // renderProducts();
+        // renderCart();
+    
